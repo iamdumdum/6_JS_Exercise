aea6fb3de681bedb007001ec634844fb0ef014bf
iamdumdum <Trevorjiang@protonmail.com>
2020-12-16 22:17:20 +0000

    done

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var name = "Alice";
 
 //only change code below this line
 
-var firstChar; //get the first character of the name
+var firstChar = name.slice(0,1); //get the first character of the name
 
-var middleChar; //get the middle character of the name
+var middleChar = name.slice(2,3); //get the middle character of the name
 
-var lastChar; //get the last character of the name
+var lastChar = name.slice(4,5); //get the last character of the name
 
 //only change code above this line
 
